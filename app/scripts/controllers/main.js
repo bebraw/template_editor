a3d6fd025051abf39fc51771dc67c68272c0a775
Juho Vepsalainen <bebraw@gmail.com>
2014-03-06 12:31:08 +0100

    Allow new elements to be created
    They will be selected by default and it is possible to modify their
    properties.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,15 +1,42 @@
 'use strict';
 
 angular.module('templateEditorApp').controller('MainCtrl', function($scope) {
+    $scope.elements = [];
     $scope.selected = {};
 
     $scope.types = ['text', 'media', 'table'];
-    $scope.selectedType = $scope.types[0];
 
     $scope.fonts = ['Arial', 'Helvetica', 'Times New Roman'];
     $scope.selectedFont = $scope.fonts[0];
 
+    $scope.addNew = addNew;
+    $scope.remove = remove;
+
     initializeWorkspace();
+
+    function addNew() {
+        var element = {
+            type: 'text',
+            x: 0.45,
+            y: 0.45,
+            z: 0,
+            w: 0.2,
+            h: 0.2
+        };
+
+        $scope.elements.push(element);
+
+        select(element);
+    }
+
+    function remove() {
+        console.log('should remove selected element');
+    }
+
+    function select(element) {
+        $scope.selected = element;
+        element.selected = true;
+    }
 
     function initializeWorkspace() {
         var workspace = document.getElementById('workspace');
