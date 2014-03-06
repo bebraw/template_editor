a37e1c35f924cb81c88adee8fbe13194f3d734d7
Juho Vepsalainen <bebraw@gmail.com>
2014-03-06 09:53:26 +0100

    Initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('templateEditorApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
