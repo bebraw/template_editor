00d63ee70d932eaca3184fedbf4603b9c6ceddcb
Juho Vepsalainen <bebraw@gmail.com>
2014-03-06 10:33:15 +0100

    Add type dependent controls

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,10 +1,11 @@
 'use strict';
 
-angular.module('templateEditorApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+angular.module('templateEditorApp').controller('MainCtrl', function($scope) {
+    $scope.selected = {};
+
+    $scope.types = ['text', 'media', 'table'];
+    $scope.selectedType = $scope.types[0];
+
+    $scope.fonts = ['Arial', 'Helvetica', 'Times New Roman'];
+    $scope.selectedFont = $scope.fonts[0];
+});
